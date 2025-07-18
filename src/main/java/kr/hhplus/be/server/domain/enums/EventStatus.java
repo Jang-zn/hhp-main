90b011e1ad0a2d3c15dc8988d6bc21b15f5b6171
Jang-zn <zn2309@naver.com>
2025-07-18 10:57:42 +0900

    fix: 패키지 경로 수정

--- src/main/java/kr/hhplus/be/server/domain/enums/EventStatus.java
+++ src/main/java/kr/hhplus/be/server/domain/enums/EventStatus.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.enum;
+package kr.hhplus.be.server.domain.enums;
 
 public enum EventStatus {
     PENDING,
--- src/main/java/kr/hhplus/be/server/domain/enums/EventType.java
+++ src/main/java/kr/hhplus/be/server/domain/enums/EventType.java
@@ -1,8 +1,6 @@
-package kr.hhplus.be.server.domain.enum;
+package kr.hhplus.be.server.domain.enums;
 
 public enum EventType {
     ORDER_CREATED,
     PAYMENT_COMPLETED,
-    BALANCE_UPDATED,
-    COUPON_APPLIED
 } 
