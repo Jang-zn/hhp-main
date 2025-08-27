5de9f509b9b7fc36e073d254dbd28607ff704b59
Jang-zn <zn2309@naver.com>
2025-08-28 00:39:37 +0900

    feat: InMemoryMessagingAdapter 삭제 및 EventPort 로 MessagePort 이름 수정

--- src/main/java/kr/hhplus/be/server/domain/port/messaging/EventPort.java
+++ src/main/java/kr/hhplus/be/server/domain/port/messaging/EventPort.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.port.messaging;
 
-public interface MessagingPort {
+public interface EventPort {
     void publish(String topic, Object event);
     void publishAsync(String topic, Object event);
 } 
